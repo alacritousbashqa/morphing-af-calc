bc887b4a5ebbb2b38817a781ccdd892342031a42
Mark Bashqawi <mbashqawi@gmail.com>
2021-02-23 10:41:15 -0600

    Add wake solver

--- wake_run.m
+++ wake_run.m
@@ -48,6 +48,7 @@ delY = (-Y_mid(I_sp-1)+Y(I_sp))/2;
 spWake(:,1) = spWake(:,1) - delX.*ones(length(spWake(:,1)),1);
 spWake(:,2) = spWake(:,2) - delY.*ones(length(spWake(:,2)),1);
 
+[Cp,strengths,X_mid,Y_mid,norms,tans] = inv_wake_sol(X,Y,Vinf,alpha,zeros(N,1),I_sp,Nw,spWake,teWake);
 
 figure
 plot(X,Y,'k.-')
@@ -142,6 +143,155 @@ A(N,M) = 1;
 A(N,I) = 1;
 b(I) = 0;
 b(M) = 0;
+
+function [Cp,strengths,X_mid,Y_mid,norms,tans] = inv_wake_sol(X,Y,Vinf,alpha,extra,I,Nw,spWake,teWake)
+
+M = length(X);          % Number of points
+N = M-1;                % Number of panels
+
+Qinf = Vinf*[cosd(alpha) sind(alpha)];  % Freestream velocity vector
+
+[X_mid,Y_mid,norms,tans] = geo_decomp(X,Y); % Acquire geoemtric properties from airfoil
+
+thetas = zeros(N,1);    % Holds all panel orientations with respect to the x-axis
+
+%% Calculate influence coefficients
+
+A = zeros(N+3,N+3);     % Influence coefficient matrix
+B = A;
+b = zeros(N+3,1);       % Freestream contribution
+
+for i = 1:N
+    % Coordinates of ith panel end and control point
+    %   Control Points: (x,y)
+    %   First End Point: (xi,yi)
+    %   Second End Point: (xii,yii)
+    xi = X(i);
+    xii = X(i+1);
+    yi = Y(i);
+    yii = Y(i+1);
+    x = (xi+xii)/2;                 % Control point x-coordinate
+    y = (yi+yii)/2;                 % Control point y-coordinate
+    
+    ti = atan2(yii-yi,xii-xi);      % Panel orientation angle [rad]
+    thetas(i) = ti;     % Store orientations
+    
+    u2 = 0;
+    v2 = 0;
+    
+    for j = 1:N
+        % Coordinates of jth panel end points
+        %   First End Point: (xj,yj)
+        %   Second End Point: (xjj,yjj)
+        xj = X(j);
+        xjj = X(j+1);
+        yj = Y(j);
+        yjj = Y(j+1);
+        
+        u2_old = u2;
+        v2_old = v2;
+        
+        % Get induced velocity at control point due to jth panel endpoints
+        % with vortex strength of 1
+        [u1,v1,u2,v2] = getVi(1,1,x,y,xj,yj,xjj,yjj,j==i);
+        
+        if j==1
+            A(i,1) = dot([u1 v1],norms(i,:));
+            B(i,1) = dot([u1 v1],tans(i,:));
+        elseif j==I
+            continue;
+        elseif j==I+1
+            A(i,j-1) = dot([u1 v1],norms(i,:));
+            A(i,j) = dot([u1 v1] + [u2_old v2_old],norms(i,:));
+            B(i,j) = dot([u1 v1] + [u2_old v2_old],tans(i,:));
+        elseif j==N
+            A(i,N) = dot([u1 v1] + [u2_old v2_old],norms(i,:));
+            A(i,M) = dot([u2 v2],norms(i,:));
+            B(i,N) = dot([u1 v1] + [u2_old v2_old],tans(i,:));
+            B(i,M) = dot([u2 v2],tans(i,:));
+        else
+            A(i,j) = dot([u1 v1] + [u2_old v2_old],norms(i,:));
+            B(i,j) = dot([u1 v1] + [u2_old v2_old],tans(i,:));
+        end
+    end
+    
+    uc = 0;
+    vc = 0;
+    for k = 1:Nw
+        xk = spWake(k,1);
+        xkk = spWake(k+1,1);
+        yk = spWake(k,2);
+        ykk = spWake(k+1,2);
+        a = atan2(ykk-yk,xkk-xk);   % Panel orientation angle [rad]
+
+        % Transformation Matrix (global to panel)
+        AA = [cos(a) sin(a);
+            -sin(a) cos(a)];
+
+        % Transform to panel coordinates
+        tmp = AA*[x-xk;y-yk];
+        xp = tmp(1);
+        yp = tmp(2);
+        S = (xkk-xk)*cos(a) + (ykk-yk)*sin(a);
+
+        % Distances and angles from enpoints j and jj to point (x,y)
+        rk = sqrt(xp^2 + yp^2);
+        rkk = sqrt((xp-S)^2 + yp^2);
+        tk = atan2(yp,xp);
+        tkk = atan2(yp,xp-S);
+
+        U = (tkk-tk)/2/pi;
+        V = -log(rk/rkk)/2/pi;
+
+        uc = uc + (-U*cos(a)-V*sin(a));
+        vc = vc + (-U*sin(a)+V*cos(a));
+    end
+    A(i,M+1) = dot([uc vc],norms(i,:));
+    
+    uc = 0;
+    vc = 0;
+    for k = 1:Nw
+        xk = teWake(k,1);
+        xkk = teWake(k+1,1);
+        yk = teWake(k,2);
+        ykk = teWake(k+1,2);
+        a = atan2(ykk-yk,xkk-xk);   % Panel orientation angle [rad]
+
+        % Transformation Matrix (global to panel)
+        AA = [cos(a) sin(a);
+            -sin(a) cos(a)];
+
+        % Transform to panel coordinates
+        tmp = AA*[x-xk;y-yk];
+        xp = tmp(1);
+        yp = tmp(2);
+        S = (xkk-xk)*cos(a) + (ykk-yk)*sin(a);
+
+        % Distances and angles from enpoints j and jj to point (x,y)
+        rk = sqrt(xp^2 + yp^2);
+        rkk = sqrt((xp-S)^2 + yp^2);
+        tk = atan2(yp,xp);
+        tkk = atan2(yp,xp-S);
+
+        U = (tkk-tk)/2/pi;
+        V = -log(rk/rkk)/2/pi;
+
+        uc = uc + (-U*cos(a)-V*sin(a));
+        vc = vc + (-U*sin(a)+V*cos(a));
+    end
+    A(i,M+2) = dot([uc vc],norms(i,:));
+    
+    % Freestream flow contribution
+    b(i) = dot(-Qinf,norms(i,:)) - extra(i);
+end
+
+% Kutta Condition + wake conditions
+A(M,M) = 1;
+A(M+1,1) = 1;
+A(M+1,M+1) = 1;
+A(M+2,M) = 1;
+A(M+2,M+2) = 1;
+
 
 %% Solve linear system for strengths
 strengths = A\b;
